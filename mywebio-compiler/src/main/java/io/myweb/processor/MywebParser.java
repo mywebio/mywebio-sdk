1cf1b8195e62f9378c1519ba18b988dd2ea2d4c9
Grzegorz Kubiak <grzegorz.kubiak@gmail.com>
2014-08-09 19:15:03 +0200

    Simplify MywebParser

--- mywebio-compiler/src/main/java/io/myweb/processor/MywebParser.java
+++ mywebio-compiler/src/main/java/io/myweb/processor/MywebParser.java
@@ -23,46 +23,87 @@ public class MywebParser extends AnnotationMessagerAware {
 	}
 
 	public ParsedMethod parse(ExecutableElement ee) {
-		String destClass = ee.getEnclosingElement().toString();
-		String destMethod = ee.getSimpleName().toString();
-		String destMethodRetType = ee.getReturnType().toString();
-		List<ParsedParam> params = new LinkedList<ParsedParam>();
-		String httpMethod = "GET";
-		String httpUri = "/";
-		String produces = MimeTypes.MIME_TEXT_PLAIN;
-		List<? extends VariableElement> parameters = ee.getParameters();
-		int i = 0;
-		for (VariableElement p : parameters) {
-			ParsedParam pp = null;
-			try {
-				String type = p.asType().toString().replaceFirst("class ", "");
-				pp = new ParsedParam(i++, type, p.getSimpleName().toString());
-			} catch (ClassNotFoundException e) {
-				error("cannot load class: " + e.getMessage());
-			}
-			params.add(pp);
-		}
-		for (AnnotationMirror am : ee.getAnnotationMirrors()) {
-			// TODO verify if annotation aren't duplicated
-			String annotationName = am.getAnnotationType().toString();
-			if (GET.class.getName().equals(annotationName) || POST.class.getName().equals(annotationName)
-					|| DELETE.class.getName().equals(annotationName) || PUT.class.getName().equals(annotationName)) {
-				httpMethod = annotationName.substring(annotationName.lastIndexOf(".") + 1).trim();
-				for (Map.Entry<? extends ExecutableElement, ? extends AnnotationValue> entry : am.getElementValues().entrySet()) {
-					if ("value".equals(entry.getKey().getSimpleName().toString())) {
-						httpUri = entry.getValue().getValue().toString();
-						mywebValidator.validateAnnotation(httpMethod, destMethodRetType, destMethod, params, httpUri, ee, am);
-					}
-				}
-			}
-			if (Produces.class.getName().equals(annotationName)) {
-				for (Map.Entry<? extends ExecutableElement, ? extends AnnotationValue> entry : am.getElementValues().entrySet()) {
-					if ("value".equals(entry.getKey().getSimpleName().toString())) {
-						produces = entry.getValue().getValue().toString();
-					}
-				}
-			}
-		}
-		return new ParsedMethod(destClass, destMethod, destMethodRetType, params, httpMethod, httpUri, produces);
+        List<ParsedParam> parasedParams = extractMethodParams(ee);
+        return extractParsedMethod(ee, parasedParams);
 	}
+
+    private ParsedMethod extractParsedMethod(ExecutableElement ee, List<ParsedParam> params) {
+        String destClass = ee.getEnclosingElement().toString();
+        String destMethod = ee.getSimpleName().toString();
+        String destMethodRetType = ee.getReturnType().toString();
+        String httpMethod = "GET";
+        String httpUri = "/";
+        String produces = MimeTypes.MIME_TEXT_PLAIN;
+        List<? extends AnnotationMirror> annotationMirrors = ee.getAnnotationMirrors();
+        for (AnnotationMirror am : annotationMirrors) {
+            // TODO verify if annotations aren't duplicated
+            httpMethod = extractHttpMethod(am);
+            httpUri = validateAndExtractHttpUri(am, httpMethod, destMethodRetType, destMethod, params, ee);
+            produces = extractProducesAnnotation(am);
+        }
+        return new ParsedMethod(destClass, destMethod, destMethodRetType, params, httpMethod, httpUri, produces);
+    }
+
+    private String validateAndExtractHttpUri(AnnotationMirror am, String httpMethod, String destMethodRetType, String destMethod, List<ParsedParam> params, ExecutableElement ee) {
+        String annotationName = am.getAnnotationType().toString();
+        String httpUri = "/";
+        if (isHttpMethodAnnotation(annotationName)) {
+            for (Map.Entry<? extends ExecutableElement, ? extends AnnotationValue> entry : am.getElementValues().entrySet()) {
+                if ("value".equals(entry.getKey().getSimpleName().toString())) {
+                    httpUri = entry.getValue().getValue().toString();
+                    mywebValidator.validateAnnotation(httpMethod, destMethodRetType, destMethod, params, httpUri, ee, am);
+                }
+            }
+        }
+        return httpUri;
+    }
+
+    private boolean isHttpMethodAnnotation(String annotationName) {
+        return GET.class.getName().equals(annotationName) || POST.class.getName().equals(annotationName)
+                || DELETE.class.getName().equals(annotationName) || PUT.class.getName().equals(annotationName);
+    }
+
+    private String extractHttpMethod(AnnotationMirror am) {
+        String annotationName = am.getAnnotationType().toString();
+        String httpMethod = "GET";
+        if (isHttpMethodAnnotation(annotationName)) {
+            httpMethod = annotationName.substring(annotationName.lastIndexOf(".") + 1).trim();
+        }
+        return httpMethod;
+    }
+
+    private String extractProducesAnnotation(AnnotationMirror am) {
+        String produces = MimeTypes.MIME_TEXT_PLAIN;
+        String annotationName = am.getAnnotationType().toString();
+        if (Produces.class.getName().equals(annotationName)) {
+            for (Map.Entry<? extends ExecutableElement, ? extends AnnotationValue> entry : am.getElementValues().entrySet()) {
+                if ("value".equals(entry.getKey().getSimpleName().toString())) {
+                    produces = entry.getValue().getValue().toString();
+                }
+            }
+        }
+        return produces;
+    }
+
+    private List<ParsedParam> extractMethodParams(ExecutableElement ee) {
+        List<? extends VariableElement> parameters = ee.getParameters();
+        List<ParsedParam> params = new LinkedList<ParsedParam>();
+        int i = 0;
+        for (VariableElement p : parameters) {
+            ParsedParam pp = extractMethodParam(i++, p);
+            params.add(pp);
+        }
+        return params;
+    }
+
+    private ParsedParam extractMethodParam(int paramNumber, VariableElement variableElement) {
+        ParsedParam pp = null;
+        try {
+            String type = variableElement.asType().toString().replaceFirst("class ", "");
+            pp = new ParsedParam(paramNumber, type, variableElement.getSimpleName().toString());
+        } catch (ClassNotFoundException e) {
+            error("cannot load class: " + e.getMessage());
+        }
+        return pp;
+    }
 }
