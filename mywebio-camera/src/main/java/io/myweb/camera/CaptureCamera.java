e032efeb01ca170edcf0b945fccfbd5dd8127fbb
Jacek Zadrąg <jacek.zadrag@javeo.eu>
2014-09-09 19:06:29 +0200

    Capture camera working example

--- mywebio-camera/src/main/java/io/myweb/camera/CaptureCamera.java
+++ mywebio-camera/src/main/java/io/myweb/camera/CaptureCamera.java
@@ -7,78 +7,65 @@ import android.hardware.Camera;
 import android.media.CamcorderProfile;
 import android.media.MediaRecorder;
 import android.util.Log;
-import io.myweb.api.GET;
-import io.myweb.api.Produces;
+
+import io.myweb.api.*;
 
 import java.io.FileDescriptor;
 import java.io.IOException;
 
-public class CaptureCamera {
+public class CaptureCamera implements ResponseCallback, MediaRecorder.OnErrorListener {
+	private static final int HIDDEN_MPEG_TS = 8;
+	private static final String LOG_TAG = CaptureCamera.class.getSimpleName();
+	private MediaRecorder mediaRecorder;
+	private Camera camera;
 
 	@GET("/camera")
-	@Produces("video/mp4")
-	public String camera(Context ctx, FileDescriptor fd) {
-		if (hasCameraHardware(ctx)) {
-			Camera c = null;
-			try {
-				c = Camera.open(); // attempt to get a Camera instance
-			}
-			catch (Exception e){
-				// Camera is not available (in use or does not exist)
-				return "";
-			}
+	public Response camera(Context ctx) {
+		if (!hasCameraHardware(ctx)) return Response.serviceUnavailable();
 
-			MediaRecorder mMediaRecorder = new MediaRecorder();
-
-			// Step 1: Unlock and set camera to MediaRecorder
-			c.unlock();
-			mMediaRecorder.setCamera(c);
-
-			// Step 2: Set sources
-//			mMediaRecorder.setAudioSource(MediaRecorder.AudioSource.CAMCORDER);
-			mMediaRecorder.setVideoSource(MediaRecorder.VideoSource.CAMERA);
-
-			// Step 3: Set a CamcorderProfile (requires API Level 8 or higher)
-			mMediaRecorder.setProfile(CamcorderProfile.get(CamcorderProfile.QUALITY_HIGH));
-
-			// Step 4: Set output file
-//			mMediaRecorder.setOutputFile(request.buildResponseWithFd().getFileDescriptor());
-			mMediaRecorder.setOutputFile(fd);
-
-			// Step 5: Set the preview output
-//			mMediaRecorder.setPreviewDisplay(mPreview.getHolder().getSurface());
-
-			// Step 6: Prepare configured MediaRecorder
-			try {
-				mMediaRecorder.prepare();
-			} catch (IllegalStateException e) {
-				Log.d("CaptureCamera", "IllegalStateException preparing MediaRecorder: ", e);
-				releaseMediaRecorder(mMediaRecorder, c);
-//				return false;
-				return "";
-			} catch (IOException e) {
-				Log.d("CaptureCamera", "IOException preparing MediaRecorder: ", e);
-				releaseMediaRecorder(mMediaRecorder, c);
-//				return false;
-				return "";
-			}
-			mMediaRecorder.start();
-			try {
-				Thread.sleep(10000);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-			return "";
+		if (getCamera() == null) {
+			// Camera is not available (in use or does not exist)
+			return Response.serviceUnavailable();
 		}
-		return "";
+
+		mediaRecorder = new MediaRecorder();
+
+		// Step 1: Unlock and set camera to MediaRecorder
+		getCamera().unlock();
+		mediaRecorder.setCamera(getCamera());
+
+		// Step 2: Set source
+		mediaRecorder.setVideoSource(MediaRecorder.VideoSource.CAMERA);
+
+		// Step 3: Set a CamcorderProfile (requires API Level 8 or higher)
+		mediaRecorder.setProfile(CamcorderProfile.get(CamcorderProfile.QUALITY_HIGH));
+
+		// Step 4: Set output format
+		mediaRecorder.setOutputFormat(HIDDEN_MPEG_TS);
+
+
+		// Step 5: Set the preview output
+//		mediaRecorder.setPreviewDisplay(mPreview.getHolder().getSurface());
+
+		return Response.ok().withContentType(MimeTypes.MIME_VIDEO_MPEG).withCallback(this);
 	}
 
-	private void releaseMediaRecorder(MediaRecorder mr, Camera c){
-		if (mr != null) {
-			mr.reset();   // clear recorder configuration
-			mr.release(); // release the recorder object
-//			mMediaRecorder = null;
-			c.lock();           // lock camera for later use
+	private Camera getCamera() {
+		if (camera == null) camera = Camera.open(); // attempt to get a Camera instance
+		return camera;
+	}
+
+	private void releaseMediaRecorder() {
+		if (mediaRecorder != null) {
+			try {
+				mediaRecorder.stop();
+			} catch (Exception e) {
+				Log.e(LOG_TAG, e.getMessage(), e);
+			}
+			mediaRecorder.reset();   // clear recorder configuration
+			mediaRecorder.release(); // release the recorder object
+			mediaRecorder = null;
+			getCamera().lock();           // lock camera for later use
 		}
 	}
 
@@ -90,5 +77,29 @@ public class CaptureCamera {
 			// no camera on this device
 			return false;
 		}
+	}
+
+	@Override
+	public void writeBody(FileDescriptor fd) {
+		// Step 6: Set output file
+		mediaRecorder.setOutputFile(fd);
+		mediaRecorder.setOnErrorListener(this);
+		try {
+			// Step 7: Prepare configured MediaRecorder
+			mediaRecorder.prepare();
+			// Step 8: Start the recording
+			mediaRecorder.start();
+		} catch (IllegalStateException e) {
+			Log.d(LOG_TAG, "IllegalStateException preparing MediaRecorder: ", e);
+			releaseMediaRecorder();
+		} catch (IOException e) {
+			Log.d(LOG_TAG, "IOException preparing MediaRecorder: ", e);
+			releaseMediaRecorder();
+		}
+	}
+
+	@Override
+	public void onError(MediaRecorder mediaRecorder, int i, int i2) {
+		releaseMediaRecorder();
 	}
 }
