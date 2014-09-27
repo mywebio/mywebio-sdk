b24cb067867122a4e807e30d2d8d36666ed43d50
Jacek Zadrąg <jacek.zadrag@javeo.eu>
2014-09-27 23:58:33 +0200

    Fixed retention policy (SOURCE) for annotations + @ContentProvier

--- mywebio-api-java/src/main/java/io/myweb/api/ContentProvider.java
+++ mywebio-api-java/src/main/java/io/myweb/api/ContentProvider.java
@@ -5,8 +5,11 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Target({ElementType.METHOD})
+import io.myweb.http.Method;
+
+@Target({ElementType.TYPE})
 @Retention(RetentionPolicy.SOURCE)
-public @interface POST {
-	String value() default "/";
+public @interface ContentProvider {
+	String value();
+	Method[] methods() default { Method.GET, Method.POST, Method.PUT, Method.DELETE };
 }
--- mywebio-api-java/src/main/java/io/myweb/api/PUT.java
+++ mywebio-api-java/src/main/java/io/myweb/api/PUT.java
@@ -5,8 +5,8 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Target({ElementType.METHOD, ElementType.FIELD})
-@Retention(RetentionPolicy.CLASS)
+@Target({ElementType.METHOD})
+@Retention(RetentionPolicy.SOURCE)
 public @interface PUT {
 	String value() default "/";
 }
